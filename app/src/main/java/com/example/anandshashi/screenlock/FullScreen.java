b9abfe22c9cc9349c1b4034b6d21963580781d26
Akash14204 <akashsengar786@gmail.com>
2018-04-05 00:39:49 +0530

    Commit on 5th April 2018 :)

---
+++ app/src/main/java/com/example/anandshashi/screenlock/FullScreen.java
@@ -0,0 +1,34 @@
+package com.example.anandshashi.screenlock;
+
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+public class FullScreen extends AppCompatActivity {
+
+    private VideoView myVideoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_full_screen);
+
+        myVideoView =  findViewById(R.id.videoView);
+        myVideoView.setVideoURI(Uri.parse(("android.resource://" + getPackageName() + "/" + R.raw.pa)));
+        myVideoView.start();
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                finish();
+            }
+        }, 2000);
+    }
+}
